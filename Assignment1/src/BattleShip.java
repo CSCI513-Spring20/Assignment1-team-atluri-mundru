b2f55d4c8b745c16288aa6453e2d2f2db214284f
abpha <abpha@10.0.0.61>
2020-02-05 19:07:05 -0600

    Startegic search implementation

---
+++ Assignment1/src/BattleShip.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+public class BattleShip {
+	
+		
+	int carrier = 1;
+	int submarine = 2;
+	
+	
+	String line;
+	List<int[][]> a_list = new ArrayList<int[][]>(); 
+	int[][] game = new int[25][25];
+	static SearchStrategy searchS;
+	
+	
+
+	public void strategy_implementation(SearchStrategy s_strategy){
+		searchS = s_strategy;
+	}	
+	
+	public List<int[][]> grid_alignment(String fileName){
+		  try {
+			  FileReader i_file = new FileReader(fileName);
+			  BufferedReader buffer_reader = new BufferedReader(i_file);
+			  for(int i=0;i<=2;i++) {
+					String l = buffer_reader.readLine();
+					StringTokenizer token = new StringTokenizer(l,"()");
+					
+					for(int j=0;j<8;j++) {
+						
+						String element = token.nextToken();
+						StringTokenizer token_firstc = new StringTokenizer(element,",");
+						int a = Integer.parseInt(token_firstc.nextToken());
+						int b = Integer.parseInt(token_firstc.nextToken());
+						if(j<5) {
+							game[a][b] = carrier;
+						}
+						else {
+							game[a][b] = submarine;
+						}
+			  }
+					
+					
+						   
+					//for (int row = 0; row < game.length; row++) {
+						//   for (int column = 0; column < game.length; column++) {
+						      //System.out.print(game[row][column]+" ");
+						  // }
+						   //System.out.println();
+						//}
+					StrategicSearch strategy = new StrategicSearch();
+					int result[]=strategy.findingshipsefficient(game);
+					//for(int s=0;s<8;s++) {
+						//System.out.print(result[s]+"    ");
+						
+					//} 
+					System.out.println("Strategy:Strategic Search");
+					System.out.println("Number of Cells Searched:"+result[8]);
+					System.out.print("Carrier Found:"+"("+result[0]+","+result[1]+")"+"to"+"("+"("+result[2]+","+result[3]+")");
+					System.out.println(" SubMarine Found:"+"("+result[4]+","+result[5]+")"+"to"+"("+"("+result[6]+","+result[7]+")");
+					//System.out.println("-------------------------------------------------");
+					for (int row = 0; row < game.length; row++) {
+						   for (int column = 0; column < game.length; column++) {
+						      game[row][column]=0;
+						   }
+						}
+			  }
+			  buffer_reader.close();
+			  }
+		  catch(Exception e) {
+		  System.out.println("Exception is "+e);
+		  }
+			return a_list;
+		}
+	public void reading_from_input(){
+		a_list = grid_alignment("src/input.txt");
+//		System.out.println(mylist);
+	}
+	
+	
+	public static void main(String[] args) {
+		BattleShip battleship = new BattleShip();
+		
+	battleship.reading_from_input();
+
+	}
+}
---
+++ Assignment1/src/StrategicSearch.java
@@ -0,0 +1,330 @@
+import java.util.*;
+public class StrategicSearch implements SearchStrategy{
+	public int[] findingshipsefficient(int game[][]){
+		int[] result=new int[4];
+		int[] coordinates=new int[9];
+		boolean carrierFound = false;
+		boolean battleshipFound = false;
+		int carrierStartIndex=0;
+		boolean checkCarrier=false;
+		boolean checkSubmarine=false;
+
+		int cells = 0;
+//		int mid=game[12][12];
+//		int count=0;
+//		while(!checkCarrier && !checkSubmarine ){
+//		// right side
+//		if(count+12<=24){
+//		if(game[12][count+12]==1 && !checkCarrier){
+//			checkCarrier=true;
+//			cells++;
+//			result[0]=12;
+//			result[1]=count+12;
+//		}
+//		if(game[12][count+12]==2  && !checkSubmarine){
+//			checkSubmarine=true;
+//			cells++;
+//			result[2]=12;
+//			result[3]=count+12;
+//		}
+//		} 
+//		// left side 
+//		if(12-count>=0){
+//		if(game[12][12-count]==1 && !checkCarrier){
+//			checkCarrier=true;
+//			cells++;
+//			result[0]=12;
+//			result[1]=12-count;
+//		}
+//		if(game[12][12-count]==2  && !checkSubmarine){
+//			checkSubmarine=true;
+//			cells++;
+//			result[2]=12;
+//			result[3]=12-count;
+//		}
+//		}
+//		// upper side 
+//		if(12-count>=0){
+//		if(game[12-count][12]==1 && !checkCarrier){
+//			checkCarrier=true;
+//			cells++;
+//			result[1]=12;
+//			result[0]=12-count;
+//		}
+//		if(game[12-count][12]==2  && !checkSubmarine){
+//			checkSubmarine=true;
+//			cells++;
+//			result[2]=12-count;
+//			result[3]=12;
+//		}
+//		}
+//		// down side
+//		if(count+12<=24){
+//		if(game[count+12][12]==1 && !checkCarrier){
+//			checkCarrier=true;
+//			cells++;
+//			result[0]=count+12;
+//			result[1]=12;
+//		}
+//		if(game[count+12][12]==2  && !checkSubmarine){
+//			checkSubmarine=true;
+//			cells++;
+//			result[2]=count+12;
+//			result[3]=12;
+//		}
+//		}
+//		// diagonal right down  
+//		if(count+12<=24){
+//		if(game[count+12][count+12]==1 && !checkCarrier){
+//			checkCarrier=true;
+//			cells++;
+//			result[0]=count+12;
+//			result[1]=count+12;
+//		}
+//		if(game[count+12][count+12]==2  && !checkSubmarine){
+//			checkSubmarine=true;
+//			cells++;
+//			result[2]=count+12;
+//			result[3]=count+12;
+//		}
+//		}
+//		// diagonal right up row-- clo++
+//		if(12-count>=0 && count+12<=24){
+//		if(game[12-count][count+12]==1 && !checkCarrier){
+//			checkCarrier=true;
+//			cells++;
+//			result[0]=12-count;
+//			result[1]=count+12;
+//		}
+//		if(game[12-count][count+12]==2  && !checkSubmarine){
+//			checkSubmarine=true;
+//			cells++;
+//			result[2]=12-count;
+//			result[3]=count+12;
+//		}
+//		}
+//		// diagonal left up row-- clo--
+//		if(12-count>=0 && count+12<=24){
+//		if(game[12-count][12-count]==1 && !checkCarrier){
+//			checkCarrier=true;
+//			cells++;
+//		}if(game[12-count][12-count]==2  && !checkSubmarine){
+//			checkSubmarine=true;
+//			cells++;
+//		}
+//
+//		// diagonal left down row++ clo--
+//		if(12-count>=0 && count+12<=24){
+//		if(game[12+count][12-count]==1 && !checkCarrier){
+//			checkCarrier=true;
+//			cells++;
+//		}
+//		if(game[12+count][12-count]==2 && !checkSubmarine ){
+//			checkSubmarine=true;
+//			cells++;
+//		}
+//		}
+//
+//
+//		}
+//count++;
+//		}
+//		checkCarrier=false;
+//		checkSubmarine=false;
+		
+//		for(int i=0;i<25;i++) {
+//			for(int j=0;j<25;j++) {
+//				if(game[i][j]==1 && !carrierFound) {
+//					result[0] = i;
+//					result[1] = j;
+//					carrierFound=true;
+//					cells++;
+//				}
+//				if(game[i][j]==2 && !battleshipFound) {
+//					result[2] = i;
+//					result[3] = j;
+//					battleshipFound=true;
+//					cells++;
+//				}
+//				if(carrierFound&&battleshipFound)break;
+//				cells++;
+//			}
+//		}
+		while(!carrierFound) {
+			Random random_number = new Random();
+			int a = random_number.nextInt(25);
+			int b = random_number.nextInt(25);
+			if (game[a][b] == 1) {
+				carrierFound = true;
+				result[0] = a;
+				result[1] = b;
+				cells++;
+				break;
+			}
+			cells++;
+		}
+		while(!battleshipFound) {
+			Random number = new Random();
+			int c = number.nextInt(25);
+			int d = number.nextInt(25);
+			if (game[c][d] == 2) {
+				battleshipFound = true;
+				result[2] = c;
+				result[3] = d;
+				cells++;
+			}
+				
+			cells++;
+			
+		}
+		if (result[1]+1<=24) {
+			if(game[result[0]][result[1]+1] == 1) {
+				for(int i=0;i<25;i++) {
+					if(game[result[0]][i]==1) {
+						carrierStartIndex=i;
+						coordinates[0]=result[0];
+						coordinates[1]=i;
+						coordinates[2]=result[0];
+						coordinates[3]=i+4;
+						cells++;
+						checkCarrier=true;
+						break;
+						
+					}
+				}
+			}
+		}
+
+		 if ( result[1]-1 >= 0 ) {
+			if(game[result[0]][result[1]-1] == 1) {
+				for(int i=0;i<25;i++) {
+					if(game[result[0]][i]==1) {
+						carrierStartIndex=i;
+						coordinates[0]=result[0];
+						coordinates[1]=i;
+						coordinates[2]=result[0];
+						coordinates[3]=i+4;
+						cells++;
+						checkCarrier=true;
+						break;
+						
+					}
+				}
+			}
+		}
+		 if ( result[0]+1<=24) {
+
+			 if(game[result[0]+1][result[1]] == 1) {
+				for(int i=0;i<25;i++) {
+					if(game[i][result[1]]==1) {
+						carrierStartIndex=i;
+						coordinates[0]=i;
+						cells++;
+						coordinates[1]=result[1];
+						coordinates[2]=i+4;
+						coordinates[3]=result[1];
+						checkCarrier=true;
+						break;						
+					}
+				}
+			}
+			
+		}
+		 if ( result[0]-1>=0 ) {
+				//System.out.println("here");
+
+			if(game[result[0]-1][result[1]] == 1) {
+				for(int i=0;i<25;i++) {
+					if(game[i][result[1]]==1) {
+						carrierStartIndex=i;
+						coordinates[0]=i;
+						cells++;
+						coordinates[1]=result[1];
+						coordinates[2]=i+4;
+						coordinates[3]=result[1];
+						checkCarrier=true;
+						break;						
+					}
+				}
+			}
+			
+		}
+					
+		 if (result[3]+1<=24) {
+				if(game[result[2]][result[3]+1] == 2) {
+					for(int i=0;i<25;i++) {
+						if(game[result[2]][i]==2) {
+							carrierStartIndex=i;
+							coordinates[4]=result[2];
+							coordinates[5]=i;
+							coordinates[6]=result[2];
+							coordinates[7]=i+2;
+							cells++;
+							checkSubmarine=true;
+							break;
+							
+						}
+					}
+				}
+			}
+
+			 if ( result[3]-1 >= 0) {
+				if(game[result[2]][result[3]-1] == 2) {
+					for(int i=0;i<25;i++) {
+						if(game[result[2]][i]==2) {
+							carrierStartIndex=i;
+							coordinates[4]=result[2];
+							coordinates[5]=i;
+							coordinates[6]=result[2];
+							coordinates[7]=i+2;
+							cells++;
+							checkSubmarine=true;
+							break;
+							
+						}
+					}
+				}
+			}
+			 if ( result[2]+1<=24) {
+				if(game[result[2]+1][result[3]] == 2) {
+					for(int i=0;i<25;i++) {
+						if(game[i][result[3]]==2) {
+							carrierStartIndex=i;
+							coordinates[4]=i;
+							coordinates[5]=result[3];
+							coordinates[6]=i+2;
+							coordinates[7]=result[3];
+							cells++;
+							checkSubmarine=true;
+							break;						
+						}
+					}
+				}
+				
+			}
+			 if ( result[2]-1>=0) {
+				if(game[result[2]-1][result[3]] == 2) {
+					for(int i=0;i<25;i++) {
+						if(game[i][result[3]]==2) {
+							carrierStartIndex=i;
+							coordinates[4]=i;
+							coordinates[5]=result[3];
+							coordinates[6]=i+2;
+							coordinates[7]=result[3];
+							checkSubmarine=true;
+							cells++;
+							break;						
+						}
+					}
+				}
+				
+			}
+
+			 coordinates[8]=cells;
+		return coordinates;
+	}
+
+	
+}
+
+
